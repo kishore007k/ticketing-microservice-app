c6a15f6f0cfefac4ca5cfc267063b13869645455
kishore <kishore.kumar@m8y.one>
2022-02-24 22:52:21 +0530

    FEAT: Add SignUp

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,10 +8,19 @@ import { signOutRouter } from "./routes/sign-out";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const app = express();
 
+app.set("trust proxy", true); // trust first proxy for https (ingress ngx)
+
 app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true, // only send cookies over https
+	})
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -26,6 +35,10 @@ app.all("*", async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+	if (!process.env.JWT_KEY) {
+		throw new Error("JWT_KEY must be defined");
+	}
+
 	try {
 		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
 	} catch (error) {
