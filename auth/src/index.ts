6c14940b14793e8d2532ab53ec1c2ea1f526f33f
kishore <kishore.kumar@m8y.one>
2022-02-25 10:40:35 +0530

    FEAT: Add Testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/sign-in";
-import { signUpRouter } from "./routes/sign-up";
-import { signOutRouter } from "./routes/sign-out";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-const app = express();
-
-app.set("trust proxy", true); // trust first proxy for https (ingress ngx)
-
-app.use(json());
-app.use(
-	cookieSession({
-		signed: false,
-		secure: true, // only send cookies over https
-	})
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all("*", async (req, res) => {
-	throw new NotFoundError();
-});
-
-// Error Handler
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
